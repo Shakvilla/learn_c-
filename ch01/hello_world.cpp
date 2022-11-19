1a1c46f39ec2e2e5daee4c662ecd8a38abc53d9b
Shakvilla <abdulshakuraclement@yahoo.com>
2022-11-19 02:38:41 +0000

    a program that prints Welcome to C++

---
+++ ch01/hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // a preprocessor directive that allows input and output
+
+
+/**
+ * main - entry point function
+*/
+int main(){
+
+	std::cout << "Welcome to C++\n";
+
+	return 0;
+}
