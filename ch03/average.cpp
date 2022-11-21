1f440c35106a838958785097647568d2fe76d851
Shakvilla <abdulshakuraclement@yahoo.com>
2022-11-21 00:59:47 +0000

    average of a class using while statement

---
+++ ch03/average.cpp
@@ -0,0 +1,27 @@
+// Solving the class-average problem using counter-controlled iteration.
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int total = 0;
+    int gradeCounter = 1;
+    int grade; 
+
+    while(gradeCounter <= 10){
+
+        cout << "Enter grade: \n";
+        cin >> grade;
+        total = total + grade;
+
+        gradeCounter = gradeCounter + 1;
+        
+    }
+
+    int average{ total / 10 };
+    cout << "\n Total of all test scores is: " << total;
+    cout << "\n the average of the class scores is: " << average << "\n"; 
+
+    return 0;
+}
